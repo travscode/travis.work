cd131bc49b70414fe6f7e71f7ce7435cd56a2adf
Travis Weerts <travisaweerts@gmail.com>
2025-04-28 14:52:41 +0800

    fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,7 @@ const eslintConfig = [
      "@typescript-eslint/no-unsafe-call": "off",
      "@typescript-eslint/no-unsafe-member-access": "off",
      "@typescript-eslint/no-unsafe-return": "off",
+     "@typescript-eslint/no-unused-vars": "off",
   },
 }),
 ];
