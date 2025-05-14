3b18cb204aa17fe4c894122317d3c8570f522d32
Travis Weerts <travisaweerts@gmail.com>
2025-05-14 15:12:43 +0800

    sitemap stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,16 +15,52 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Travis Weerts | Award-winning Perth based digital designer, developer, and creative consultant",
+  title: "Travis Weerts | Award-winning designer, developer, and creative in Perth",
   description: "Helping startups ditch the dull and launch with style—think killer design, sharp dev, and brands that actually get noticed. Boring? Nope, not here.",
   authors: [{ name: "Travis Weerts", url: "https://travis.work" }],
+  keywords: [
+    "Travis Weerts",
+    "design",
+    "developer",
+    "creative",
+    "Perth",
+    "startup",
+    "app developer",
+    "web developer",
+    "web designer",
+    "app designer",
+    "UI/UX designer",
+    "UI designer",
+    "UX designer",
+    "digital designer",
+    "digital developer",
+    "digital creative",
+    "digital agency",
+    "digital agency Perth",
+    "digital agency in Perth",
+    "digital agency Perth WA",
+  ],
+  themeColor: "#000000",
+  colorScheme: "dark",
   openGraph: {
-    title: "Travis Weerts | Award-winning Perth based digital designer, developer, and creative consultant",
+    title: "Travis Weerts | Award-winning designer, developer, and creative in Perth",
     description:
       "Helping startups ditch the dull and launch with style—think killer design, sharp dev, and brands that actually get noticed. Boring? Nope, not here.",
     images: ["https://travis.work/cover-image.jpg"],
     type: "website",
+    url: "https://travis.work",
+    siteName: "Travis Weerts",
+    locale: "en_AU",
   },
+  twitter: {
+    title: "Travis Weerts | Award-winning designer, developer, and creative in Perth",
+    description:
+      "Helping startups ditch the dull and launch with style—think killer design, sharp dev, and brands that actually get noticed. Boring? Nope, not here.",
+    images: ["https://travis.work/cover-image.jpg"],
+    card: "summary_large_image",
+    creator: "@travisweerts",
+    site: "@travisweerts",
+  }
 };
 
 export default function RootLayout({
